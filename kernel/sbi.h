a6de86b1052b4b2d22dcb3edc8ee28a0eb423b41
saviopereira-ask <ailamariabarbosa1975@gmail.com>
2026-03-04 14:55:27 -0300

    add endif for sbi.h

--- kernel/sbi.h
+++ kernel/sbi.h
@@ -29,5 +29,5 @@ struct sbiret sbi_call( long eid,long fid,
                         long arg0,long arg1,
                         long arg2,long arg3,
                         long arg4,long arg5);
-
+#endif 
 
