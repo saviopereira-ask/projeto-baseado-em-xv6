1db5256e30bf8ff4951f3beae888278eea5edca7
saviopereira-ask <ailamariabarbosa1975@gmail.com>
2026-03-06 17:38:27 -0300

    low change

--- sbi/calls/sbi_reset.c
+++ sbi/calls/sbi_reset.c
@@ -3,7 +3,8 @@
 struct sbiret sbi_call_cold_reboot(void) {
 
     return sbi_call( SBI_EID_RESET,SBI_FID_RESET,
-                     SBI_SRST_COLD_REBOOT,0,0,0,0,0);
+                     SBI_SRST_COLD_REBOOT,
+0,0,0,0,0);
 }
 
 
